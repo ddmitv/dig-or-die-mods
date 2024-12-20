eadaa435daf6a6dde0f3a90f56c3e9c3429419e6
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-12-20 13:46:41 +0500

    Create a script for generating a texture spritesheet

---
+++ more-items/make-textures.ps1
@@ -0,0 +1,19 @@
+$textures = @(
+    "flashLightMK3.png",
+    "miniaturizorMK6_icon.png",
+    "miniaturizorMK6.png",
+    "betterPotionHpRegen.png",
+    "defenseShieldMK2.png",
+    "waterBreatherMK2.png",
+    "jetpackMK2.png",
+    "antiGravityWall.png",
+    "turretReparatorMK3_unit.png",
+    "turretReparatorMK3_icon.png",
+    "turretReparatorMK3.png",
+    "megaExplosive.png",
+    "turretParticlesMK2_icon.png",
+    "turretParticlesMK2_unit.png",
+    "turretTeslaMK2.png"
+)
+
+magick $($textures.foreach({"textures/$_"})) +append "Resources/textures"
