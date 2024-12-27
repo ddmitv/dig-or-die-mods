c2cfcf4c0020b1b5ec0642af1b227f7033d1afb1
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-12-27 17:42:48 +0500

    [more-items] Fix inventory sorting for custom items

--- more-items/Patches.cs
+++ more-items/Patches.cs
@@ -388,4 +388,34 @@ public class Patches {
         }
         return true;
     }
+    [HarmonyPatch(typeof(CInventory), "InventorySorting")]
+    [HarmonyPrefix]
+    private static bool CInventory_InventorySorting(CStack a, CStack b, ref int __result) {
+        static int categoryToOrdinal(string categoryId) {
+            if (!categoryId.StartsWith("CITEM_")) { return 7; }
+            return categoryId.Substring(6) switch {
+                "DEVICE" => 0,
+                "WEAPON" => 1,
+                "DEFENSE" => 2,
+                "WALL" => 3,
+                "MACHINE" => 4,
+                "MINERAL" => 5,
+                "MATERIAL" => 6,
+                _ => 7,
+            };
+        }
+        const ushort lastItemId = 201;
+
+        var a_item = a.m_item;
+        var b_item = b.m_item;
+        if (a_item.m_id > lastItemId || b_item.m_id > lastItemId) {
+            if (a_item.m_categoryId == b_item.m_categoryId) {
+                __result = a_item.m_id - b_item.m_id;
+            } else {
+                __result = categoryToOrdinal(a_item.m_categoryId) - categoryToOrdinal(b_item.m_categoryId);
+            }
+            return false;
+        }
+        return true;
+    }
 }
