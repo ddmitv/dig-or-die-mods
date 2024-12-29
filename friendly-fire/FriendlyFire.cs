144401386e1af313a6cd25a193371319b2dc62b0
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-12-29 23:46:53 +0500

    [friendly-fire] Remove unused `using` directive

--- friendly-fire/FriendlyFire.cs
+++ friendly-fire/FriendlyFire.cs
@@ -1,10 +1,7 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using BepInEx.Logging;
 using HarmonyLib;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
