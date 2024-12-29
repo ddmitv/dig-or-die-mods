6e38af3fe507adf7d0d86ceb10164f06021cff9f
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-12-29 23:43:59 +0500

    [friendly-fire] Add ability to hide player names & chat messages above their heads

--- friendly-fire/FriendlyFire.cs
+++ friendly-fire/FriendlyFire.cs
@@ -30,18 +30,24 @@ public static class Utils {
 [BepInPlugin("friendly-fire", "Friendly Fire", "1.0.0")]
 public class FriendlyFire : BaseUnityPlugin
 {
-    private void Awake()
+    private void Start()
     {
-        Harmony.CreateAndPatchAll(typeof(FriendlyFire));
-
         ConfigEntry<bool> configDamageAOE = Config.Bind<bool>(
             section: "FriendlyFire", key: "DamageAOE", defaultValue: true,
             description: "Enables damage for players from explosions/lightning"
         );
+        ConfigEntry<bool> configHideNames = Config.Bind<bool>(
+            section: "FriendlyFire", key: "HideNames", defaultValue: false,
+            description: "Hides other player names and chat messages above their heads"
+        );
+
+        Harmony.CreateAndPatchAll(typeof(FriendlyFire));
         if (configDamageAOE.Value) {
             Harmony.CreateAndPatchAll(typeof(SUnits_DoDamageAOE_Patch));
         }
-        System.Console.WriteLine("Plugin \"Friendly Fire\" is loaded!");
+        if (configHideNames.Value) {
+            Harmony.CreateAndPatchAll(typeof(HidePlayerNames_Patch));
+        }
     }
 
     [HarmonyTranspiler]
@@ -63,7 +69,7 @@ public class FriendlyFire : BaseUnityPlugin
                 new CodeMatch(OpCodes.Ldloc_2),
                 new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(List<CUnit>), nameof(List<CUnit>.Contains))),
                 new CodeMatch(OpCodes.Brtrue))
-            .ThrowIfInvalid("friendly-fire transpiler: Failed to find `ldarg.0`, `ldfld CBullet.m_unitsHit`, `ldloc.2`, `callvirt List.Contains`")
+            .ThrowIfInvalid("friendly-fire transpiler (1)")
             .Advance(1)
             .Inject(OpCodes.Ldarg_0)
             .CreateLabel(out var failLabel)
@@ -152,19 +158,50 @@ public static class SUnits_DoDamageAOE_Patch {
                 new CodeMatch(OpCodes.Ldarg_S, (byte)8),
                 new CodeMatch(OpCodes.Ldc_R4, -3.4028235E+38f),
                 new CodeMatch(OpCodes.Beq))
-            .ThrowIfInvalid("friendly-fire transpiler: Failed to find `ldarg.s angleMin`, `ldc.r4 -3.4028235E+38f`, `beq`")
+            .ThrowIfInvalid("friendly-fire transpiler (1)")
             .CreateLabel(out var successLabel);
 
         codeMatcher.Start()
             .MatchForward(useEnd: true,
                 new CodeMatch(OpCodes.Ldarg_S, (byte)6),
                 new CodeMatch(OpCodes.Brfalse))
-            .ThrowIfInvalid("friendly-fire transpiler: Failed to find `ldarg.s isFromPlayer`, `brfalse`")
+            .ThrowIfInvalid("friendly-fire transpiler (2)")
             .Advance(1)
             .Inject(OpCodes.Ldloc_2)
             .Insert(
                 new CodeInstruction(OpCodes.Isinst, typeof(CUnitPlayer)),
                 new CodeInstruction(OpCodes.Brtrue, successLabel));
+
+        return codeMatcher.Instructions();
+    }
+}
+
+public static class HidePlayerNames_Patch {
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(SScreenHudWorld), "OnUpdate")]
+    private static IEnumerable<CodeInstruction> SScreenHudWorld_OnUpdate(IEnumerable<CodeInstruction> instructions) {
+        var codeMatcher = new CodeMatcher(instructions);
+        var CMeshText_Get = AccessTools.Method(typeof(CMesh<CMeshText>), nameof(CMesh<CMeshText>.Get), [typeof(SScreen), typeof(bool)]);
+
+        codeMatcher.Start()
+            .MatchForward(useEnd: false,
+                new CodeMatch(OpCodes.Ldarg_0),
+                new CodeMatch(OpCodes.Ldc_I4_0),
+                new CodeMatch(OpCodes.Call, CMeshText_Get))
+            .ThrowIfInvalid("friendly-fire transpiler (1)")
+            .SetAndAdvance(OpCodes.Nop, null)
+            .RemoveInstructions(28);
+        codeMatcher
+            .MatchForward(useEnd: false,
+                new CodeMatch(OpCodes.Ldarg_0),
+                new CodeMatch(OpCodes.Ldc_I4_0),
+                new CodeMatch(OpCodes.Call, CMeshText_Get),
+                new CodeMatch(OpCodes.Ldloc_S),
+                new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(CPlayer), nameof(CPlayer.m_lastChat))),
+                new CodeMatch(OpCodes.Ldloca_S))
+            .ThrowIfInvalid("friendly-fire transpiler (2)")
+            .SetAndAdvance(OpCodes.Nop, null)
+            .RemoveInstructions(22);
 
         return codeMatcher.Instructions();
     }
