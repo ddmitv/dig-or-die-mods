3adcd6866d07e43ff1d7d2d35bd3092a43522ddb
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2025-01-10 05:26:18 +0500

    [debug-mode] Add all debug settings + live settings update

--- debug-mode/DebugMode.cs
+++ debug-mode/DebugMode.cs
@@ -1,7 +1,9 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
 using ModUtils;
 using Mono.Cecil;
+using System;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using UnityEngine;
@@ -34,11 +36,68 @@ public static class EnableDebugModePatch {
 [BepInPlugin("debug-mode", "Debug Mode", "1.0.0")]
 public class DebugMode : BaseUnityPlugin
 {
+    private void OnChangedSetting() {
+        var configDrawAllBackgrounds = Config.Bind<bool>(
+            section: "Debug", key: "drawAllBackgrounds", defaultValue: false
+        );
+        var configBullets = Config.Bind<bool>(
+            section: "Debug", key: "bullets", defaultValue: false
+        );
+        var configPathfinding = Config.Bind<bool>(
+            section: "Debug", key: "pathfinding", defaultValue: false
+        );
+        var configPathfindingDetails = Config.Bind<bool>(
+            section: "Debug", key: "pathfindingDetails", defaultValue: false
+        );
+        var configCollisions = Config.Bind<bool>(
+            section: "Debug", key: "collisions", defaultValue: false
+        );
+        var configUnits = Config.Bind<bool>(
+            section: "Debug", key: "units", defaultValue: false
+        );
+        var configUnitNetworkControl = Config.Bind<bool>(
+            section: "Debug", key: "unitNetworkControl", defaultValue: false
+        );
+        var configDefenses = Config.Bind<bool>(
+            section: "Debug", key: "defenses", defaultValue: false
+        );
+        var configWater = Config.Bind<bool>(
+            section: "Debug", key: "water", defaultValue: false
+        );
+        var configLight = Config.Bind<bool>(
+            section: "Debug", key: "light", defaultValue: false
+        );
+        var configCrashes = Config.Bind<bool>(
+            section: "Debug", key: "crashes", defaultValue: false
+        );
+        var configCrashesFull = Config.Bind<bool>(
+            section: "Debug", key: "crashesFull", defaultValue: false
+        );
+        G.m_debugDrawAllBackgrounds = configDrawAllBackgrounds.Value;
+        G.m_debugBullets = configBullets.Value;
+        G.m_debugPF = configPathfinding.Value;
+        G.m_debugPFDetails = configPathfindingDetails.Value;
+        G.m_debugCols = configCollisions.Value;
+        G.m_debugUnits = configUnits.Value;
+        G.m_debugUnitNetworkControl = configUnitNetworkControl.Value;
+        G.m_debugDefenses = configDefenses.Value;
+        G.m_debugWater = configWater.Value;
+        G.m_debugLight = configLight.Value;
+        G.m_debugCrashes = configCrashes.Value;
+        G.m_debugCrashesFull = configCrashesFull.Value;
+    }
+
     private void Start() {
         Utils.UniqualizeVersionBuild(ref G.m_versionBuild, this);
 
         var harmony = new Harmony("debug-mode");
+
         harmony.PatchAll(typeof(EnableDebugModePatch));
+
+        OnChangedSetting();
+        Config.SettingChanged += (object _, SettingChangedEventArgs _) => {
+            OnChangedSetting();
+        };
     }
 }
 
