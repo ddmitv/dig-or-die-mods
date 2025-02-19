a4529fe8951a394aa9167135a93059d358b33dc3
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2025-02-19 15:45:36 +0500

    Add new plugin `enable-asserts`

---
+++ plugins/enable-asserts/EnableAsserts.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Linq;
+
+public static class AssertPatch {
+    [HarmonyPatch(typeof(SMisc), nameof(SMisc.Assert), [typeof(bool), typeof(string)])]
+    [HarmonyPrefix]
+    private static bool SMisc_Assert(bool test, string errorMessage) {
+        if (test) { return false; }
+        EnableAsserts.AssertFail(errorMessage);
+        return true;
+    }
+    [HarmonyPatch(typeof(SMisc), nameof(SMisc.Assert), [typeof(bool)])]
+    [HarmonyPrefix]
+    private static bool SMisc_Assert(bool test) {
+        if (test) { return false; }
+        EnableAsserts.AssertFail("Assertation failed");
+        return true;
+    }
+}
+
+[BepInPlugin("enable-asserts", "Enable Asserts", "1.0.0")]
+public class EnableAsserts : BaseUnityPlugin
+{
+    public static ManualLogSource Log = null;
+    public static ConfigEntry<bool> configIsFatal = null;
+
+    public static void AssertFail(string message) {
+        var stacktrace = new System.Diagnostics.StackTrace(3, true);
+        Log.LogFatal($"{message}\n{stacktrace.ToString()}");
+        if (configIsFatal.Value) {
+            Environment.Exit(1);
+        }
+    }
+
+    private void Awake() {
+        Log = base.Logger;
+
+        configIsFatal = Config.Bind<bool>(section: "General", key: "IsFatal", defaultValue: true);
+
+        var harmony = new Harmony("enable-asserts");
+        harmony.PatchAll(typeof(AssertPatch));
+    }
+}
+
