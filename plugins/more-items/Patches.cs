9c0c1f491cd24c06ba6a63cc1162fbc6ffd1c4b5
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2025-05-30 17:36:12 +0500

    [more-items] Make item `RTG` damage units around it

--- plugins/more-items/Patches.cs
+++ plugins/more-items/Patches.cs
@@ -912,5 +912,30 @@ public class Patches {
             extDesc.DoEnergyExplosion(__instance);
         }
     }
+
+    private static Dictionary<ushort, double> lastRadiationHitDict = new();
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(CUnit), nameof(CUnit.Update))]
+    private static void CUnit_Update_Prefix(CUnit __instance) {
+        const int EffectRadius = 15;
+        const float RadiationDamage = 10f;
+
+        if (!__instance.IsAlive() || GVars.m_simuTimeD <= lastRadiationHitDict.GetValueSafe(__instance.m_id)) { return; }
+
+        int2 pos = Utils.FindInCircleClamped(range: EffectRadius, __instance.PosCell, (int x, int y) => {
+            return SWorld.Grid[x, y].GetContent() == CustomItems.RTG.Item;
+        });
+        if (pos == int2.negative) { return; }
+
+        lastRadiationHitDict[__instance.m_id] = GVars.m_simuTimeD + 0.5;
+
+        float distanceFactor = 1f - (pos - __instance.PosCell).sqrMagnitude / (float)(EffectRadius * EffectRadius);
+        if (__instance is CUnitPlayer) {
+            __instance.Damage(distanceFactor * RadiationDamage, showDamage: true);
+        } else {
+            __instance.Damage(distanceFactor * RadiationDamage / 3f);
+        }
+    }
 }
 
