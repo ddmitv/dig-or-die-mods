143ef61ad22a56bee0fcf8e8cb399063b68c99b6
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2025-06-20 00:08:13 +0500

    Add powershell script for helping creation of new plugins

---
+++ plugins/create-plugin.ps1
@@ -0,0 +1,36 @@
+param ($name)
+
+$pluginPath = "$PSScriptRoot/$name"
+$assemblyName = $name.Replace("-", "_")
+$productName = [cultureinfo]::GetCultureInfo("en-US").TextInfo.ToTitleCase($name.Replace("-", " "))
+$pluginName = $productName.Replace(" ", "")
+
+New-Item -ItemType Directory -Path $pluginPath
+
+Set-Content -Path "$pluginPath/.gitignore" -Value @"
+/*
+
+!.gitignore
+!$name.csproj
+!Plugin.cs
+"@
+
+Set-Content -Path "$pluginPath/$name.csproj" -Value @"
+<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <AssemblyName>$assemblyName</AssemblyName>
+    <Product>$productName</Product>
+    <Version>0.0.0</Version>
+  </PropertyGroup>
+  <Import Project="../utils-lib/utils-lib.targets"/>
+</Project>
+"@
+
+Set-Content -Path "$pluginPath/Plugin.cs" -Value @"
+using BepInEx;
+
+[BepInPlugin("$name", "$productName", "0.0.0")]
+public class $pluginName : BaseUnityPlugin {
+
+}
+"@
