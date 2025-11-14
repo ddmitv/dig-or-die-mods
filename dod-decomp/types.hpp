e5846b5e6b646b5415f22535023c72399bb39cd8
Dima Dmitriev <61628949+ddmitv@users.noreply.github.com>
2025-11-14 21:08:10 +0500

    [dod-decomp] Add more field offset hints and new `ElecSwitchType` enum

--- dod-decomp/types.hpp
+++ dod-decomp/types.hpp
@@ -11,9 +11,9 @@ using DelegateCallbackDebug = void __cdecl(const char* str);
 using DelegateCallbackGetElecProd = int __cdecl(int i, int j);
 
 struct Color24 {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
+    uint8_t r; // 0x00
+    uint8_t g; // 0x01
+    uint8_t b; // 0x02
 };
 static_assert(sizeof(Color24) == 3);
 
@@ -40,6 +40,14 @@ enum CCell_Flag : uint32_t {
 };
 DEFINE_ENUM_FLAG_OPERATORS(CCell_Flag); // macro from WinAPI
 
+enum class ElecSwitchType : int {
+    None = 0,
+    ElecCross = 1,
+    ElecSwitchRelay = 2,
+    ElecSwitch = 3,
+    ElecSwitchPush = 4,
+};
+
 struct CCell {
     CCell_Flag m_flags;      // 0x00
     uint16_t m_contentId;    // 0x04
@@ -55,70 +63,76 @@ struct CCell {
 static_assert(sizeof(CCell) == 24);
 
 struct CItem_PluginData {
-    float m_weight;            // 0x00
-    int m_electricValue;       // 0x04
-    int m_electricOutletFlags; // 0x08
-    int m_elecSwitchType;      // 0x0C
-    int m_elecVariablePower;   // 0x10
-    int m_anchor;              // 0x14
-    Color24 m_light;           // 0x18
-    int m_isBlock;             // 0x1C
-    int m_isBlockDoor;         // 0x20
-    int m_isReceivingForces;   // 0x24
-    int m_isMineral;           // 0x28
-    int m_isDirt;              // 0x2C
-    int m_isPlant;             // 0x30
-    int m_isFireProof;         // 0x34
-    int m_isWaterGenerator;    // 0x38
-    int m_isWaterPump;         // 0x3C
-    int m_isLightGenerator;    // 0x40
-    int m_isBasalt;            // 0x44
-    int m_isLightonium;        // 0x48
-    int m_isOrganicHeart;      // 0x4C
-    int m_isSunLamp;           // 0x50
-    int m_isAutobuilder;       // 0x54
-    float m_customValue;       // 0x58
+    float m_weight;                  // 0x00
+    int m_electricValue;             // 0x04
+    int m_electricOutletFlags;       // 0x08
+    ElecSwitchType m_elecSwitchType; // 0x0C
+    int m_elecVariablePower;         // 0x10
+    int m_anchor;                    // 0x14
+    Color24 m_light;                 // 0x18
+    int m_isBlock;                   // 0x1C
+    int m_isBlockDoor;               // 0x20
+    int m_isReceivingForces;         // 0x24
+    int m_isMineral;                 // 0x28
+    int m_isDirt;                    // 0x2C
+    int m_isPlant;                   // 0x30
+    int m_isFireProof;               // 0x34
+    int m_isWaterGenerator;          // 0x38
+    int m_isWaterPump;               // 0x3C
+    int m_isLightGenerator;          // 0x40
+    int m_isBasalt;                  // 0x44
+    int m_isLightonium;              // 0x48
+    int m_isOrganicHeart;            // 0x4C
+    int m_isSunLamp;                 // 0x50
+    int m_isAutobuilder;             // 0x54
+    float m_customValue;             // 0x58
 };
 static_assert(sizeof(CItem_PluginData) == 92);
 
 struct int2 {
-    int x;
-    int y;
+    int x; // 0x00
+    int y; // 0x04
 };
 static_assert(sizeof(int2) == 8);
 
+struct short2 {
+    short x; // 0x00
+    short y; // 0x02
+};
+static_assert(sizeof(short2) == 4);
+
 struct RectInt {
-    int x;
-    int y;
-    int width;
-    int height;
+    int x;      // 0x00
+    int y;      // 0x04
+    int width;  // 0x08
+    int height; // 0x12
 };
 static_assert(sizeof(RectInt) == 16);
 
 struct ThreadData {
-    HANDLE handle;                     // 0x00
-    int id;                            // 0x04
-    HANDLE workEvent;                  // 0x08
-                                       
-    bool shouldExit;                   // 0x0C
-    bool processCellLighting;          // 0x0D
-    uint8_t _padding_0E[2];            // 0x0E
-                                       
-    int startX;                        // 0x10
-    int startY;                        // 0x14
-    int endX;                          // 0x18
-    int endY;                          // 0x1C
-                                       
-    bool processVerticalWater;         // 0x20
-    uint8_t padding_21[3];             // 0x21
-                                       
-    int waterParam1;                   // 0x24
-    int waterParam2;                   // 0x1C
-                                       
-    bool processHorizontalFlow;        // 0x2C
-    uint8_t padding_2D[3];             // 0x2D
-    int flowStartOffset;               // 0x30
-    int flowIterations;                // 0x34
+    HANDLE handle;              // 0x00
+    int id;                     // 0x04
+    HANDLE workEvent;           // 0x08
+                                
+    bool shouldExit;            // 0x0C
+    bool processCellLighting;   // 0x0D
+    uint8_t _padding_0E[2];     // 0x0E
+                                
+    int startX;                 // 0x10
+    int startY;                 // 0x14
+    int endX;                   // 0x18
+    int endY;                   // 0x1C
+                                
+    bool processVerticalWater;  // 0x20
+    uint8_t padding_21[3];      // 0x21
+                                
+    int waterParam1;            // 0x24
+    int waterParam2;            // 0x1C
+                                
+    bool processHorizontalFlow; // 0x2C
+    uint8_t padding_2D[3];      // 0x2D
+    int flowStartOffset;        // 0x30
+    int flowIterations;         // 0x34
 
     uint8_t _padding[512]; // large padding (used to avoid false sharing?)
 };
