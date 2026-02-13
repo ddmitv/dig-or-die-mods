bae8bb614de4497927fbe546e554e64526114f1d
Dima Dmitriev <61628949+ddmitv@users.noreply.github.com>
2026-02-13 21:54:48 +0500

    [save-tool] Clean up namespace usings and remove `<EnableUnsafeBinaryFormatterSerialization>` in .csproj

--- tools/save-tool/Main.cs
+++ tools/save-tool/Main.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading;
 
 namespace SaveTool;
 
--- tools/save-tool/SaveConverters.cs
+++ tools/save-tool/SaveConverters.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Buffers.Binary;
 using SaveTool.Data;
 
