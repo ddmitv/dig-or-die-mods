9027f3b338a897089733137b7c3c13a0de449b08
Dima Dmitriev <61628949+ddmitv@users.noreply.github.com>
2026-07-25 19:00:16 +0500

    [AssetPacker] Fix typos in csharp keyword list

--- plugins/dodmodapi/AssetPacker/PackSpritesheet.cs
+++ plugins/dodmodapi/AssetPacker/PackSpritesheet.cs
@@ -919,12 +919,15 @@ retryWithLargerAtlas:
         }
 
         private static readonly HashSet<string> CSharpKeywords = [
-            "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue",
-            "decimal", "default", "delegate", "do", "double", "else", "enumevent", "explicit", "extern", "false", "finally", "fixed",
-            "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is", "lock", "longnamespace",
-            "new", "null", "object", "operator", "out", "override", "params", "private", "protected", "public", "readonly", "ref",
-            "return", "sbyte", "sealed", "short", "sizeof", "stackallocstatic", "string", "struct", "switch", "this", "throw", "true",
-            "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while"
+            "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked",
+            "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else",
+            "enum", "event", "explicit", "extern", "false", "finally", "fixed", "float", "for",
+            "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is",
+            "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override",
+            "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte",
+            "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch",
+            "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe",
+            "ushort", "using", "virtual", "void", "volatile", "while"
         ];
 
         private static string SanitizeIdentifier(string name) {
