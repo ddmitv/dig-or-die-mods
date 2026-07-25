9f65f0e66ecb6d83565d8a510ecd96255b71878f
Dima Dmitriev <61628949+ddmitv@users.noreply.github.com>
2026-07-25 18:06:06 +0500

    [dodmodapi] Use `Misc.AddLocalizationText` instead of raw dictionary insertion

--- plugins/dodmodapi/ItemManager.cs
+++ plugins/dodmodapi/ItemManager.cs
@@ -150,7 +150,7 @@ public static class ItemManager {
 
                 // just in case someone calls SLoc.ReprocessTexts() later, reconstruct localization strings
                 // (don't really need to do this since m_name and m_desc are inited at this point)
-                SLoc.Inst.m_dico.Add(item.m_locTextId, new SLoc.CSentence(item.m_locTextId, $"{item.m_name}|{item.m_desc}"));
+                Misc.AddLocalizationText(item.m_locTextId, $"{item.m_name}|{item.m_desc}");
             }
             // need to firstly lock item registration to disallow adding new items within OnPostModItemsInit
             _itemsLocked = true;
